ca819353486564dfd276996edaae4fbb20defcb3
hangsman <hi@hangsman.cn>
2022-04-12 12:25:25 +0800

    chore: init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Hello')
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <img class="h-200rpx w-200rpx mt-200rpx mx-auto mb-50rpx" src="/static/logo.png">
+    <div class="flex justify-center">
+      <span class="text-36rpx text-hex-8f8f94">
+        {{ title }}
+      </span>
+    </div>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import WindiCSS from 'vite-plugin-windicss'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [uni(), WindiCSS()],
+})
