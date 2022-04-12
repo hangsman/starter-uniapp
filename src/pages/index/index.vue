65eec8e6627aae3450126d115e8d262eeaab1017
hangsman <hi@hangsman.cn>
2022-04-12 22:58:17 +0800

    chore: alias config

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useCounterStore } from '@/stores/counter'
 const title = ref('Hello')
+
+const countStore = useCounterStore()
+const { increment } = useCounterStore()
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center">
     <img class="h-200rpx w-200rpx mt-200rpx mx-auto mb-50rpx" src="/static/logo.png">
-    <div class="flex justify-center">
+    <div class="flex justify-center items-center flex-col">
       <span class="text-36rpx text-hex-8f8f94">
-        {{ title }}
+        {{ countStore.count }}
       </span>
+      <button class="mt-20rpx" @click="increment()">
+        add
+      </button>
     </div>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias: {
+    '@/': `${path.resolve(__dirname, 'src')}/`,
+  },
   plugins: [uni(), WindiCSS()],
 })
